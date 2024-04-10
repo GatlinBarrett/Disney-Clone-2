0185754fa5916007fbf2ef4f987cfcc8dfcd4ac6
Gatlin <wwgdb18@gmail.com>
2024-04-10 18:25:44 -0500

    trying to fix google log in

--- app/Logout.js
+++ app/Logout.js
@@ -34,34 +34,6 @@ function Logout() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="p-8 rounded-lg shadow-md">
-        {session && (
-          <div className="hidden ml-10 md:flex items-center space-x-6">
-            <a className="header-link group">
-              <HomeIcon className="h-4" />
-              <span className="span">Home</span>
-            </a>
-            <a className="header-link group">
-              <SearchIcon className="h-4" />
-              <span className="span">Search</span>
-            </a>
-            <a className="header-link group">
-              <PlusIcon className="h-4" />
-              <span className="span">Watchlist</span>
-            </a>
-            <a className="header-link group">
-              <StarIcon className="h-4" />
-              <span className="span">Originals</span>
-            </a>
-            <a className="header-link group">
-              <img src="/images/movie-icon.svg" alt="" className="h-5" />
-              <span className="span">Movies</span>
-            </a>
-            <a className="header-link group">
-              <img src="/images/series-icon.svg" alt="" className="h-5" />
-              <span className="span">Series</span>
-            </a>
-          </div>
-        )}
         <img
           src={
             "https://lh3.googleusercontent.com/a/ACg8ocKvXNeRF59at4Lygiu_w487lfZZZ8Iu1uDJnYL2dPeHU44wLg=s96-c"
